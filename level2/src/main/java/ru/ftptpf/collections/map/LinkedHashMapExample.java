fdcddad46be41ffbadf59019e7c9ad23c9c3a0f6
Andrey Boyko <inboxru@yandex.ru>
2025-02-03 17:09:10 +0300

    LinkedHashMap

---
+++ level2/src/main/java/ru/ftptpf/collections/map/LinkedHashMapExample.java
@@ -0,0 +1,22 @@
+package ru.ftptpf.collections.map;
+
+import ru.ftptpf.collections.equals.Person;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LinkedHashMapExample {
+
+    public static void main(String[] args) {
+        Person ivan = new Person(1, "Ivan", "Ivanov");
+        Person petr = new Person(29, "Petr", "Petrov");
+        Person sveta = new Person(3, "Sveta", "Svetikova");
+
+        Map<Integer, Person> map = new LinkedHashMap<>();
+        map.put(ivan.getId(), ivan);
+        map.put(petr.getId(), petr);
+        map.putIfAbsent(sveta.getId(), sveta);
+
+        System.out.println(map.entrySet());
+    }
+}
