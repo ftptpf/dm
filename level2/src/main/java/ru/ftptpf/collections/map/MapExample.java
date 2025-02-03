acf27bb4fce5f219be94f1758668af1444ddd132
Andrey Boyko <inboxru@yandex.ru>
2025-02-03 16:59:39 +0300

    Map practice

--- level2/src/main/java/ru/ftptpf/collections/map/MapExample.java
+++ level2/src/main/java/ru/ftptpf/collections/map/MapExample.java
@@ -9,12 +9,24 @@ public class MapExample {
 
     public static void main(String[] args) {
         Person ivan = new Person(1, "Ivan", "Ivanov");
-        Person petr = new Person(2, "Petro", "Petrov");
+        Person petr = new Person(2, "Petr", "Petrov");
 
         Map<Integer, Person> map = new HashMap<>();
         map.put(ivan.getId(), ivan);
         map.put(petr.getId(), petr);
 
-        System.out.println(map.get(1));
+        System.out.println(map.keySet());
+        System.out.println(map.values());
+        System.out.println(map.entrySet());
+
+        for (Person person : map.values()) {
+            System.out.println(person.getFirstName());
+        }
+
+        for (Map.Entry<Integer, Person> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
+        System.out.println(map.containsKey(4));
     }
 }
