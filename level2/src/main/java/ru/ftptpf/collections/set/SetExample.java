208498620b2a3af53ad46e7ba40d37fb1c5da0d2
Andrey Boyko <inboxru@yandex.ru>
2025-02-04 09:09:31 +0300

    Set

---
+++ level2/src/main/java/ru/ftptpf/collections/set/SetExample.java
@@ -0,0 +1,18 @@
+package ru.ftptpf.collections.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetExample {
+
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("aaa");
+        set.add("bbb");
+        set.add("ccc");
+        set.add("ddd");
+        set.add("bbb");
+
+        System.out.println(set);
+    }
+}
