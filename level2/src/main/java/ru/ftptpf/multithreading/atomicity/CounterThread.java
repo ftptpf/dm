2b8b1c0589e439d0a7a62f4e77e9f93988d730b4
Andrey Boyko <inboxru@yandex.ru>
2025-02-17 08:50:14 +0300

    Example of non-atomic operation of threads

---
+++ level2/src/main/java/ru/ftptpf/multithreading/atomicity/CounterThread.java
@@ -0,0 +1,22 @@
+package ru.ftptpf.multithreading.atomicity;
+
+/**
+ * Создать класс CounterThread с одним полем и конструктором для инициализации:
+ * private Counter counter
+ * В методе run этого класса 100 раз вызывать метод increment
+ */
+public class CounterThread extends Thread {
+
+    private final Counter counter;
+
+    public CounterThread(Counter counter) {
+        this.counter = counter;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10000; i++) {
+            counter.increment();
+        }
+    }
+}
