1635b088342ec4527a361a550aa51ae592c29191
Andrey Boyko <inboxru@yandex.ru>
2025-02-20 08:05:29 +0300

    Atomic

---
+++ level2/src/main/java/ru/ftptpf/concurrent/atomic/AtomicDemo.java
@@ -0,0 +1,15 @@
+package ru.ftptpf.concurrent.atomic;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicDemo {
+
+    public static void main(String[] args) {
+        AtomicInteger atomicInteger = new AtomicInteger();
+        int value = atomicInteger.incrementAndGet();
+        System.out.println(value);
+
+        int newValue = atomicInteger.addAndGet(20);
+        System.out.println(newValue);
+    }
+}
