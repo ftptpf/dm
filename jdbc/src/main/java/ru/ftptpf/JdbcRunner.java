b7691363f57b70ac13f96da32b9b9739902bea40
Andrey Boyko <inboxru@yandex.ru>
2025-03-26 09:19:40 +0300

    Statement DDL operation

--- jdbc/src/main/java/ru/ftptpf/JdbcRunner.java
+++ jdbc/src/main/java/ru/ftptpf/JdbcRunner.java
@@ -4,11 +4,22 @@ import ru.ftptpf.util.ConnectionManager;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
-        try (Connection connection = ConnectionManager.open()) {
+        String sql = """
+                CREATE TABLE IF NOT EXISTS info(
+                id SERIAL PRIMARY KEY,
+                date TEXT NOT NULL);
+                """;
+        try (Connection connection = ConnectionManager.open();
+             Statement statement = connection.createStatement()) {
             System.out.println(connection.getTransactionIsolation());
+            System.out.println(connection.getSchema());
+            boolean executeResult = statement.execute(sql);
+            System.out.println(executeResult);
+
         }
     }
 }
