06323f2fb14ad35f7a631bc4ad3e54c137e82784
Andrey Boyko <inboxru@yandex.ru>
2025-03-26 08:17:21 +0300

    JDBC connection

---
+++ jdbc/src/main/java/ru/ftptpf/util/ConnectionManager.java
@@ -0,0 +1,23 @@
+package ru.ftptpf.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class ConnectionManager {
+
+    private ConnectionManager() {
+    }
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/flight_repository";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "password";
+
+    public static Connection open() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
