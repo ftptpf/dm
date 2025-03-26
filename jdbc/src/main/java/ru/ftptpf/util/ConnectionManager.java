c3cb410675afdb0714bd04a13c0418d608911cdb
Andrey Boyko <inboxru@yandex.ru>
2025-03-26 08:47:24 +0300

    JDBC properties file

--- jdbc/src/main/java/ru/ftptpf/util/ConnectionManager.java
+++ jdbc/src/main/java/ru/ftptpf/util/ConnectionManager.java
@@ -9,13 +9,17 @@ public final class ConnectionManager {
     private ConnectionManager() {
     }
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/flight_repository";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "password";
+    private static final String URL_KEY = "db.url";
+    private static final String USERNAME_KEY = "db.username";
+    private static final String PASSWORD_KEY = "db.password";
 
     public static Connection open() {
         try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            return DriverManager.getConnection(
+                    PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(USERNAME_KEY),
+                    PropertiesUtil.get(PASSWORD_KEY)
+            );
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
