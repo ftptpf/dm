b73cd5712356975e121e3a6a35721c4f16612901
Andrey Boyko <inboxru@yandex.ru>
2025-04-06 21:38:02 +0300

    Entity

--- hibernate/src/main/java/ru/ftptpf/HibernateRunner.java
+++ hibernate/src/main/java/ru/ftptpf/HibernateRunner.java
@@ -3,17 +3,28 @@ package ru.ftptpf;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import ru.ftptpf.entity.User;
+
+import java.time.LocalDate;
 
 public class HibernateRunner {
 
     public static void main(String[] args) {
-/*        BlockingQueue<Connection> pool = null;
-        Connection connection = DriverManager.getConnection("db.url", "db.username", "db.password");*/
         Configuration configuration = new Configuration();
-        /*        configuration.configure("hibernate.properties");*/
+        configuration.addAnnotatedClass(User.class);
+
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
-            System.out.println("Session started: " + session.isConnected());
+            session.beginTransaction();
+            User user = User.builder()
+                    .username("sveta@gmail.com")
+                    .firstname("Sveta")
+                    .lastname("Svetikova")
+                    .birthDay(LocalDate.of(1999, 2, 12))
+                    .age(25)
+                    .build();
+            session.persist(user);
+            session.getTransaction().commit();
         }
     }
 }
--- hibernate/src/main/resources/list.sql
+++ hibernate/src/main/resources/list.sql
@@ -1,11 +1,10 @@
 --Database: hibernate_dm_db
 
-
 CREATE TABLE IF NOT EXISTS users
 (
 username VARCHAR(128) PRIMARY KEY,
 firstname VARCHAR(128),
 lastname VARCHAR(128),
 birth_day DATE,
-age INT,
+age INT
 );
