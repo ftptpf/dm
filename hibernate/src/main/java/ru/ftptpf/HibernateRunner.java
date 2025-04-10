3b99a75d4daf041064b4f43ee877bbe3e5f91148
Andrey Boyko <inboxru@yandex.ru>
2025-04-10 09:35:09 +0300

    Session methods: save, update, saveOrUpdate, delete, get

--- hibernate/src/main/java/ru/ftptpf/HibernateRunner.java
+++ hibernate/src/main/java/ru/ftptpf/HibernateRunner.java
@@ -4,11 +4,10 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import ru.ftptpf.entity.Birthday;
+import ru.ftptpf.entity.Role;
 import ru.ftptpf.entity.User;
 
 import java.time.LocalDate;
-
-import static ru.ftptpf.entity.Role.ADMIN;
 
 public class HibernateRunner {
 
@@ -26,7 +25,7 @@ public class HibernateRunner {
                     .firstname("Sveta")
                     .lastname("Svetikova")
                     .birthDay(new Birthday(LocalDate.of(1999, 2, 12)))
-                    .role(ADMIN)
+                    .role(Role.ADMIN)
                     .info("""
                             {
                                 "name": "Sveta",
@@ -34,7 +33,17 @@ public class HibernateRunner {
                             }
                             """)
                     .build();
-            session.persist(user);
+            /*
+             * save = persist - добавляет объект в базу данных
+             * update = merge - обновляет объект в базе данных
+             * saveOrUpdate = persist если entity is transient ИЛИ merge если entity is detached
+             * delete = remove - удаляет объект из базы данных
+             * get = find - возвращает объект из базы данных
+             */
+/*            session.merge(user);*/
+/*            session.remove(user);*/
+            User sveta = session.find(User.class, "sveta@gmail.com");
+            System.out.println(sveta.getUsername());
 
             session.getTransaction().commit();
         }
