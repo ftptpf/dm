98c88ed1bf5fed067ea1d6ac11eb44eac9d4ad7c
Andrey Boyko <inboxru@yandex.ru>
2025-04-11 08:46:12 +0300

    First level cache

--- hibernate/src/main/java/ru/ftptpf/HibernateRunner.java
+++ hibernate/src/main/java/ru/ftptpf/HibernateRunner.java
@@ -42,8 +42,26 @@ public class HibernateRunner {
              */
 /*            session.merge(user);*/
 /*            session.remove(user);*/
-            User sveta = session.find(User.class, "sveta@gmail.com");
-            System.out.println(sveta.getUsername());
+/*            session.persist(user);*/
+            User sveta1 = session.find(User.class, "sveta@gmail.com");
+            /*
+            * Три варианта удаления нашей сущности из кеша 1-го уровня:
+            * 1. session.evict(sveta1);
+            * 2. session.clear();
+            * 3. session.close();
+            * Кеш 1го уровня привязан к сессии. Как правило, он чистится при закрытии сессии.
+            * Намеренно чистить его не нужно.
+            * Все изменения сущностей в рамках сессии отразятся в базе данных
+            * (в обращениях к базе данных через SQL запросы).
+            * Это будет Dirty session.
+            * Flush - все изменения в сущностях в кеше 1го уровня в рамках одной сессии сливаются в базу данных.
+            * Persistence Context сессии = кеш 1го уровня сессии.
+             */
+            session.evict(sveta1);
+            User sveta2 = session.find(User.class, "sveta@gmail.com");
+
+            System.out.println(sveta1.getUsername());
+            System.out.println(sveta2.getUsername());
 
             session.getTransaction().commit();
         }
