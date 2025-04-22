4273abcca2e53c2952c8d2af0bf5905804ce6907
Andrey Boyko <inboxru@yandex.ru>
2025-04-22 08:40:15 +0300

    HQL part 2

--- hibernate/src/test/java/ru/ftptpf/HqlTest.java
+++ hibernate/src/test/java/ru/ftptpf/HqlTest.java
@@ -21,8 +21,15 @@ public class HqlTest {
             List<User> list = session.createQuery(
                     "select u from User u where u.personalInfo.firstname = :firstname", User.class)
                     .setParameter("firstname", "Сергей")
+                    .setFetchSize(50)
                     .list();
-            System.out.println(list.size());;
+            System.out.println(list.size());
+
+            int countRows = session.createQuery("update User u set u.role = 'USER'", User.class)
+                    .executeUpdate();
+            System.out.println(countRows);
+
+            session.createNativeQuery("select u.* from User u", User.class);
 
             session.getTransaction().commit();
         }
