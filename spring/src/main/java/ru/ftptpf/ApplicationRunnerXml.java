2d012151a1c118e5e6caaf64a5ba46a9d10fceb7
Andrey Boyko <inboxru@yandex.ru>
2025-04-27 17:28:38 +0300

    XML-based Configuration

---
+++ spring/src/main/java/ru/ftptpf/ApplicationRunnerXml.java
@@ -0,0 +1,13 @@
+package ru.ftptpf;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.ftptpf.database.pool.ConnectionPool;
+
+public class ApplicationRunnerXml {
+
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        System.out.println(context.getBean("pool1", ConnectionPool.class));
+        System.out.println(context.getBean("pool2", ConnectionPool.class));
+    }
+}
