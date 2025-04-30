c31ea398bd91e73d511cc7d293a29280bc8cfc88
Andrey Boyko <inboxru@yandex.ru>
2025-04-30 09:23:06 +0300

    Annotation Autowired and Value

--- spring/src/main/java/ru/ftptpf/database/repository/CompanyRepository.java
+++ spring/src/main/java/ru/ftptpf/database/repository/CompanyRepository.java
@@ -1,20 +1,29 @@
 package ru.ftptpf.database.repository;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import ru.ftptpf.bpp.Auditing;
 import ru.ftptpf.bpp.InjectBean;
 import ru.ftptpf.bpp.Transaction;
 import ru.ftptpf.database.entity.Company;
 import ru.ftptpf.database.pool.ConnectionPool;
 
+import java.util.List;
 import java.util.Optional;
 
 @Transaction
 @Auditing
 public class CompanyRepository implements CrudRepository<Integer, Company> {
 
-    @InjectBean
+    @Autowired
+    @Qualifier("pool1")
     private ConnectionPool pool;
+    @Autowired
+    private List<ConnectionPool> pools;
+    @Value("${db.pool.size}")
+    private Integer poolSize;
 
     @PostConstruct
     private void init() {
