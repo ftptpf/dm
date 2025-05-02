8e3f692d5648fdcc46b2724cd5d81568a5f31187
Andrey Boyko <inboxru@yandex.ru>
2025-05-02 21:34:12 +0300

    Properties

--- spring-boot/src/main/java/ru/ftptpf/ApplicationRunner.java
+++ spring-boot/src/main/java/ru/ftptpf/ApplicationRunner.java
@@ -9,6 +9,7 @@ public class ApplicationRunner {
     public static void main(String[] args) {
 
         SpringApplication.run(ApplicationRunner.class, args);
+        System.out.println("Application started");
     }
 
 }
