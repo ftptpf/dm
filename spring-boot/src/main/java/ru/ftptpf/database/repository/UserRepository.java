b31e76bd9f2471b12ad3937df93abf5c0ae6c290
Andrey Boyko <inboxru@yandex.ru>
2025-05-11 12:55:06 +0300

    Annotation Modifying

--- spring-boot/src/main/java/ru/ftptpf/database/repository/UserRepository.java
+++ spring-boot/src/main/java/ru/ftptpf/database/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package ru.ftptpf.database.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import ru.ftptpf.database.entity.Role;
 import ru.ftptpf.database.entity.User;
 
 import java.util.List;
@@ -15,5 +17,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query(value = "SELECT u.* FROM users u WHERE u.username = :username",
             nativeQuery = true)
     List<User> findAllByUsername(@Param("firstname") String username);
+
+    @Modifying(clearAutomatically = true)
+    @Query("update User u set u.role = :role where u.id in (:ids)")
+    int updateRole(@Param("role") Role role, @Param("ids") Long... ids);
 
 }
--- spring-boot/src/test/java/ru/ftptpf/integration/database/repository/UserRepositoryIT.java
+++ spring-boot/src/test/java/ru/ftptpf/integration/database/repository/UserRepositoryIT.java
@@ -3,8 +3,8 @@ package ru.ftptpf.integration.database.repository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.test.annotation.Rollback;
+import ru.ftptpf.database.entity.Role;
 import ru.ftptpf.database.entity.User;
 import ru.ftptpf.database.repository.UserRepository;
 import ru.ftptpf.integration.annotation.IT;
@@ -19,9 +19,21 @@ class UserRepositoryIT {
     private UserRepository userRepository;
 
     @Test
+    void checkUpdate() {
+        User ivan = userRepository.getReferenceById(1L);
+        Assertions.assertThat(ivan.getRole()).isEqualTo(Role.ADMIN);
+
+        int resultCount = userRepository.updateRole(Role.USER, 1L, 5L);
+        Assertions.assertThat(resultCount).isEqualTo(2);
+
+        User theSameIvan = userRepository.getReferenceById(1L);
+        Assertions.assertThat(theSameIvan.getRole()).isEqualTo(Role.USER);
+    }
+
+    @Test
     void checkQueries() {
         List<User> users = userRepository.findAllBy("a", "ov");
         Assertions.assertThat(users).hasSize(3);
-
     }
+
 }
