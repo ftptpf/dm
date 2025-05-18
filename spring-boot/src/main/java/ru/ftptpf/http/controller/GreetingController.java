0bc4588052ba4e9ac66a0a3e5d6250503afb6ab4
Andrey Boyko <inboxru@yandex.ru>
2025-05-18 21:27:54 +0300

    Annotation Request Mapping

--- spring-boot/src/main/java/ru/ftptpf/http/controller/GreetingController.java
+++ spring-boot/src/main/java/ru/ftptpf/http/controller/GreetingController.java
@@ -1,16 +1,20 @@
 package ru.ftptpf.http.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GreetingController {
 
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public ModelAndView hello(ModelAndView modelAndView) {
         modelAndView.setViewName("greeting/hello");
         return modelAndView;
     }
 
+    @RequestMapping(value = "/bye", method = RequestMethod.GET)
     public ModelAndView bye(ModelAndView modelAndView) {
         modelAndView.setViewName("greeting/bye");
         return modelAndView;
