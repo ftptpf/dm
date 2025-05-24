b5a432d18de1def0582560bedcfbf73c791d6700
Andrey Boyko <inboxru@yandex.ru>
2025-05-24 20:08:54 +0300

    Model

---
+++ spring-boot/src/main/java/ru/ftptpf/dto/UserReadDto.java
@@ -0,0 +1,9 @@
+package ru.ftptpf.dto;
+
+import lombok.Value;
+
+@Value
+public class UserReadDto {
+    Long id;
+    String username;
+}
--- spring-boot/src/main/java/ru/ftptpf/http/controller/GreetingController.java
+++ spring-boot/src/main/java/ru/ftptpf/http/controller/GreetingController.java
@@ -5,19 +5,29 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import ru.ftptpf.dto.UserReadDto;
 
 import java.util.Map;
 
 @Controller
 @RequestMapping("/api/v1")
+@SessionAttributes({"user"})
 public class GreetingController {
 
-    @GetMapping("/hello/{id}")
+    @GetMapping("/hello")
     public ModelAndView hello(ModelAndView modelAndView,
-                              HttpServletRequest request,
-                              @RequestParam("age") Integer age,
-                              @RequestHeader("User-Agent") String userAgent,
-                              @PathVariable("id") Integer id) {
+                              HttpServletRequest request) {
+        modelAndView.setViewName("greeting/hello");
+        modelAndView.addObject("user", new UserReadDto(1L, "Ivan"));
+        return modelAndView;
+    }
+
+    @GetMapping("/hello2/{id}")
+    public ModelAndView hello2(ModelAndView modelAndView,
+                               HttpServletRequest request,
+                               @RequestParam("age") Integer age,
+                               @RequestHeader("User-Agent") String userAgent,
+                               @PathVariable("id") Integer id) {
         Map<String, String[]> parameterMap = request.getParameterMap();
         Cookie[] cookies = request.getCookies();
         modelAndView.setViewName("greeting/hello");
@@ -25,7 +35,8 @@ public class GreetingController {
     }
 
     @GetMapping("/bye")
-    public ModelAndView bye(ModelAndView modelAndView) {
+    public ModelAndView bye(ModelAndView modelAndView,
+                            @SessionAttribute("user") UserReadDto user) {
         modelAndView.setViewName("greeting/bye");
         return modelAndView;
     }
