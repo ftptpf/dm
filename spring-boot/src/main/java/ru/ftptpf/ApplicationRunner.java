b77e16aaae4a64bc8327c4cccf07f8967d092360
Andrey Boyko <inboxru@yandex.ru>
2025-05-24 19:32:04 +0300

    Parameters, Headers, Cookies

--- spring-boot/src/main/java/ru/ftptpf/ApplicationRunner.java
+++ spring-boot/src/main/java/ru/ftptpf/ApplicationRunner.java
@@ -1,17 +1,12 @@
 package ru.ftptpf;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
 @SpringBootApplication
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-
         SpringApplication.run(ApplicationRunner.class, args);
-        log.info("Application started");
     }
-
 }
--- spring-boot/src/main/java/ru/ftptpf/http/controller/GreetingController.java
+++ spring-boot/src/main/java/ru/ftptpf/http/controller/GreetingController.java
@@ -1,20 +1,30 @@
 package ru.ftptpf.http.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Map;
+
 @Controller
+@RequestMapping("/api/v1")
 public class GreetingController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public ModelAndView hello(ModelAndView modelAndView) {
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello(ModelAndView modelAndView,
+                              HttpServletRequest request,
+                              @RequestParam("age") Integer age,
+                              @RequestHeader("User-Agent") String userAgent,
+                              @PathVariable("id") Integer id) {
+        Map<String, String[]> parameterMap = request.getParameterMap();
+        Cookie[] cookies = request.getCookies();
         modelAndView.setViewName("greeting/hello");
         return modelAndView;
     }
 
-    @RequestMapping(value = "/bye", method = RequestMethod.GET)
+    @GetMapping("/bye")
     public ModelAndView bye(ModelAndView modelAndView) {
         modelAndView.setViewName("greeting/bye");
         return modelAndView;
