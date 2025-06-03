4ac6b0542268c4c7e048322457e635d521de7d73
Andrey Boyko <inboxru@yandex.ru>
2025-06-03 09:21:43 +0300

    CRUD View Layer part 2

--- spring-boot/src/main/java/ru/ftptpf/http/controller/LoginController.java
+++ spring-boot/src/main/java/ru/ftptpf/http/controller/LoginController.java
@@ -5,9 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ru.ftptpf.dto.LoginDto;
 
 @Controller
+@RequestMapping("/api/v1")
 public class LoginController {
 
     @GetMapping("/login")
--- spring-boot/src/main/java/ru/ftptpf/http/controller/UserController.java
+++ spring-boot/src/main/java/ru/ftptpf/http/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.ftptpf.database.entity.Role;
 import ru.ftptpf.dto.UserCreateEditDto;
 import ru.ftptpf.service.CompanyService;
@@ -37,9 +38,23 @@ public class UserController {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
+    @GetMapping("/users/registration")
+    public String registration(Model model, @ModelAttribute("user") UserCreateEditDto user) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        model.addAttribute("companies", companyService.findAll());
+        return "/user/registration";
+    }
+
     @PostMapping("/users")
 //    @ResponseStatus(HttpStatus.CREATED)
-    public String create(@ModelAttribute("user") UserCreateEditDto user) {
+    public String create(@ModelAttribute("user") UserCreateEditDto user, RedirectAttributes redirectAttributes) {
+/*        if (true) {
+            redirectAttributes.addAttribute("user", user.getUsername());
+            redirectAttributes.addAttribute("firstname", user.getFirstname());
+            redirectAttributes.addFlashAttribute("user", user);
+            return "redirect:/api/v1/users/registration";
+        }*/
         return "redirect:/api/v1/users/" + userService.create(user).getId();
     }
 
