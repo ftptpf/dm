48ef50df94f787fd3cefd489c32c7059e74757fb
Andrey Boyko <inboxru@yandex.ru>
2025-06-06 09:17:49 +0300

    REST Практика 1

--- spring-boot/src/main/java/ru/ftptpf/http/handler/ControllerExceptionHandler.java
+++ spring-boot/src/main/java/ru/ftptpf/http/handler/ControllerExceptionHandler.java
@@ -3,16 +3,14 @@ package ru.ftptpf.http.handler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
-@ControllerAdvice
-public class ControllerExceptionHandler /* extends ResponseEntityExceptionHandler*/ {
+@ControllerAdvice(basePackages = "ru.ftptpf.http.controller")
+public class ControllerExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public String handleExceptions(Exception exception) {
         log.error("Failed to return response");
         return "error/error500";
     }
-
 }
---
+++ spring-boot/src/main/java/ru/ftptpf/http/rest/UserRestController.java
@@ -0,0 +1,56 @@
+package ru.ftptpf.http.rest;
+
+import jakarta.validation.groups.Default;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import ru.ftptpf.dto.UserCreateEditDto;
+import ru.ftptpf.dto.UserReadDto;
+import ru.ftptpf.service.UserService;
+import ru.ftptpf.validation.group.CreateAction;
+import ru.ftptpf.validation.group.UpdateAction;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v2/users")
+@RequiredArgsConstructor
+public class UserRestController {
+
+    private final UserService userService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<UserReadDto> findAll() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public UserReadDto findById(@PathVariable("id") Long id) {
+        return userService.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserReadDto create(@Validated({Default.class, CreateAction.class}) @RequestBody UserCreateEditDto user) {
+        return userService.create(user);
+    }
+
+    @PutMapping("/{id}")
+    public UserReadDto update(@PathVariable("id") Long id,
+                              @Validated({Default.class, UpdateAction.class}) @RequestBody UserCreateEditDto user) {
+        return userService.update(id, user)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") Long id) {
+        if (!userService.delete(id)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+}
