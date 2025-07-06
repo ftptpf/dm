4e3732b0a57bfbcd12f8fce94f4709bf1bcea21f
Andrey Boyko <inboxru@yandex.ru>
2025-07-06 21:08:03 +0300

    AOP Pointcut

---
+++ spring-boot/src/main/java/ru/ftptpf/aop/FirstAspect.java
@@ -0,0 +1,74 @@
+package ru.ftptpf.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class FirstAspect {
+
+    /**
+     * @within - check annotation on the class level
+     */
+    @Pointcut("@within(org.springframework.stereotype.Controller)")
+    public void isControllerLayer() {
+    }
+
+    /**
+     * within - check class type name
+     */
+    @Pointcut("within(ru.ftptpf.service.*Service)")
+    public void isServiceLayer() {
+    }
+
+    /**
+     * this - check AOP proxy class type
+     * target - check target object class type
+     */
+    @Pointcut("this(org.springframework.data.repository.Repository)")
+//    @Pointcut("target(org.springframework.data.repository.Repository)")
+    public void isRepositoryLayer() {
+    }
+
+    /**
+     * @annotation - check annotation on method level
+     */
+    @Pointcut("isControllerLayer() && @annotation(org.springframework.web.bind.annotation.GetMapping)")
+    public void hasGetMapping() {
+    }
+
+    /**
+     * args - check method param type
+     * - any param type
+     * .. - 0+ any params type
+     */
+    @Pointcut("isControllerLayer() && args(org.springframework.ui.Model,..)")
+    public void hasModelParam() {
+    }
+
+    /**
+    @args - check annotation on the param type
+    * - any param type
+    .. - 0+ any params type
+ */
+    @Pointcut("isControllerLayer() && @args(ru.ftptpf.validation.UserInfo,..)")
+    public void hasUserInfoParamAnnotation() {
+    }
+
+    /**
+        bean - check bean name
+     */
+    @Pointcut("bean(*Service)")
+    public void isServiceLayerBean() {
+    }
+
+    /**
+        execution(modifiers-pattern? ret-type-pattern declaring-type-pattern?name-pattern(param-pattern) throws-pattern?)
+     */
+    @Pointcut("execution(public * ru.ftptpf.service.*Service.findById(*))")
+    public void anyFindByIdServiceMethod() {
+    }
+
+
+}
